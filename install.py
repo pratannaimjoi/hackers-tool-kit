97c4dadddc1d1c938796d2763945faf03e74cbcc
Pratan naimjoi <52064201+tanpattaya@users.noreply.github.com>
2019-10-18 12:52:44 +0700

    Update install.py

--- install.py
+++ install.py
@@ -31,7 +31,7 @@ def install():
 	os.system("gem install lolcat")
 	os.system("cd /root/ && git clone https://github.com/Und3rf10w/kali-anonsurf")
 	os.system("cd /root/kali-anonsurf && ./installer.sh")
-	print "y\n"
+	print "apt update\n"
 	print """entering big download region prepare you anus
 	if your not ready press ctrl C """
 	i = raw_input("press ctrl c to stop hit enter to continue")
